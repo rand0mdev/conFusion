2dd79822f283e8a092d1ed1998c4fe4ad40aba3a
rand0mdev <bechiirr71@gmail.com>
2020-11-01 23:32:12 +0000

    feat(spa): added menu details in navigation route

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,19 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
+import { Location } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DishService } from "../services/dish.service";
+import { Dish } from "../shared/dish";
 
 @Component({
-  selector: 'app-dishdetail',
-  templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  selector: "app-dishdetail",
+  templateUrl: "./dishdetail.component.html",
+  styleUrls: ["./dishdetail.component.scss"],
 })
 export class DishdetailComponent implements OnInit {
-
-  @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    const { id } = this.route.snapshot.params;
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Promotion } from '../shared';
+import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 
 @Injectable({
