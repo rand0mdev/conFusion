6286734cd242ce6c61b3e88d2b8d9af0f338b349
rand0mdev <bechiirr71@gmail.com>
2020-11-01 22:29:01 +0000

    feat: added routing module

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
